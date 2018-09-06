c09457541e0037b3bb87d8d2a04d6c3fff6f63d4
Alejandro Sanchez <emailforasr@gmail.com>
2018-09-06 16:33:00 -0700

    NRMS-3049 Openshift Build and Deployment Configuration
    * Changes to `settings.sh`
    * Ignore temporary, auto-generated openshift files

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,17 +1,15 @@
-# bash script to be sourced to set environment variables for OpenShift scripts
-export PROJECT_NAMESPACE="nrms-api"
-export PROJECT_OS_DIR=${PROJECT_OS_DIR:-../../openshift}
-
-# The templates that should not have their GIT references(uri and ref) over-ridden
-# Templates NOT in this list will have they GIT references over-ridden
-# with the values of GIT_URI and GIT_REF
-export skip_git_overrides=""
-export GIT_URI="https://github.com/bcgov/gcpe-NRMS-api.git"
-export GIT_REF="develop"
+export PROJECT_NAMESPACE=${PROJECT_NAMESPACE:-nrms}
+export GIT_URI=${GIT_URI:-"https://github.com/bcgov/gcpe-NRMS-api.git"}
+export GIT_REF=${GIT_REF:-"develop"}
 
 # The project components
-# - defaults to the support the Simple Project Structure
-export components=${components:-"Gcpe.Hub.NRMS"}
+# - They are all contained under the main OpenShift folder.
+export components=${components:-"."}
+
+# The templates that should not have their GIT referances(uri and ref) over-ridden
+# Templates NOT in this list will have they GIT referances over-ridden
+# with the values of GIT_URI and GIT_REF
+export skip_git_overrides=""
 
 # The builds to be triggered after buildconfigs created (not auto-triggered)
 export builds=${builds:-""}
